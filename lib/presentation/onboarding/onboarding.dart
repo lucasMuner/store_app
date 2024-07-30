3a69d3f53c82f425a73c1463c9ad9c42dcb33faa
lucasMuner <lucas.muner0622@gmail.com>
2024-07-29 21:49:10 -0300

    Applying MVVM 2

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_advanced_app/presentation/onboarding/onboarding_view_model.dart';
 import 'package:flutter_advanced_app/presentation/resources/assets_manager.dart';
 import 'package:flutter_advanced_app/presentation/resources/color_manager.dart';
 import 'package:flutter_advanced_app/presentation/resources/strings_manager.dart';
 import 'package:flutter_advanced_app/presentation/resources/values_manager.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../domain/model.dart';
 import '../resources/routes_manager.dart';
 
 class OnBoardingView extends StatefulWidget {
@@ -16,154 +18,146 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
-  late final List<SliderObject> _list = _getSliderData();
-
   PageController _pageController = PageController(initialPage: 0);
+  OnBoardingViewModel _viewModel = OnBoardingViewModel();
 
-  int _currentIndex = 0;
+  _bind() {
+    _viewModel.start();
+  }
 
-  List<SliderObject> _getSliderData() => [
-        SliderObject(
-          AppString.onBoardingTitle1,
-          AppString.onBoardingSubTitle1,
-          ImageAssets.onboardingLogo1,
-        ),
-        SliderObject(
-          AppString.onBoardingTitle2,
-          AppString.onBoardingSubTitle2,
-          ImageAssets.onboardingLogo2,
-        ),
-        SliderObject(
-          AppString.onBoardingTitle3,
-          AppString.onBoardingSubTitle3,
-          ImageAssets.onboardingLogo3,
-        ),
-      ];
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorManager.white,
-      appBar: AppBar(
+    return StreamBuilder<SlideViewObject>(
+        stream: _viewModel.outputSliderViewObject,
+        builder: (context, response) => _getContentWidget(response.data));
+  }
+
+  Widget _getContentWidget(SlideViewObject? slideViewObject) {
+    if (slideViewObject == null) {
+      return Container();
+    } else {
+      return Scaffold(
         backgroundColor: ColorManager.white,
-        elevation: AppSize.s0,
-        systemOverlayStyle: SystemUiOverlayStyle(
-          statusBarColor: ColorManager.white,
-          statusBarBrightness: Brightness.dark,
-          statusBarIconBrightness: Brightness.dark,
+        appBar: AppBar(
+          backgroundColor: ColorManager.white,
+          elevation: AppSize.s0,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarColor: ColorManager.white,
+            statusBarBrightness: Brightness.dark,
+            statusBarIconBrightness: Brightness.dark,
+          ),
         ),
-      ),
-      body: PageView.builder(
-          controller: _pageController,
-          itemCount: _list.length,
-          onPageChanged: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          itemBuilder: (context, index) {
-            return OnBoardingPage(_list[index]);
-          }),
-      bottomSheet: Container(
-        color: ColorManager.white,
-        height: AppSize.s100,
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, Routes.loginRoute);
-                },
-                child: Text(
-                  AppString.skip,
-                  textAlign: TextAlign.end,
+        body: PageView.builder(
+            controller: _pageController,
+            itemCount: slideViewObject.numOfSlides,
+            onPageChanged: (index) {
+              _viewModel.onPageChanged(index);
+            },
+            itemBuilder: (context, index) {
+              return OnBoardingPage(slideViewObject.sliderObject);
+            }),
+        bottomSheet: Container(
+          color: ColorManager.white,
+          height: AppSize.s100,
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, Routes.loginRoute);
+                  },
+                  child: Text(
+                    AppString.skip,
+                    textAlign: TextAlign.end,
+                  ),
                 ),
               ),
-            ),
-            _getBottomSheetPage()
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _getBottomSheetPage() {
-    return Container(
-      color: ColorManager.primary,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Padding(
-            //left arrow
-            padding: EdgeInsets.all(AppPadding.p14),
-            child: GestureDetector(
-              child: SizedBox(
-                height: AppSize.s20,
-                width: AppSize.s20,
-                child: SvgPicture.asset(ImageAssets.leftArrowIc),
-              ),
-              onTap: () {
-                _pageController.animateToPage(_getPreviousIndex(),
-                    duration: Duration(milliseconds: DurationConstant.d300),
-                    curve: Curves.bounceInOut);
-              },
-            ),
-          ),
-
-          //circles indicator
-          Row(
-            children: [
-              for (int i = 0; i < _list.length; i++)
-                Padding(
-                  padding: EdgeInsets.all(AppPadding.p14),
-                  child: _getProperCircle(i),
-                )
+              _getBottomSheetPage(slideViewObject)
             ],
           ),
-          //right arrow
-          Padding(
-            //left arrow
-            padding: EdgeInsets.all(AppPadding.p14),
-            child: GestureDetector(
-              child: SizedBox(
-                height: AppSize.s20,
-                width: AppSize.s20,
-                child: SvgPicture.asset(ImageAssets.rightArrowIc),
+        ),
+      );
+    }
+  }
+
+  Widget _getBottomSheetPage(SlideViewObject? slideViewObject) {
+    if (slideViewObject == null) {
+      return Container();
+    } else {
+      return Container(
+        color: ColorManager.primary,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              //left arrow
+              padding: EdgeInsets.all(AppPadding.p14),
+              child: GestureDetector(
+                child: SizedBox(
+                  height: AppSize.s20,
+                  width: AppSize.s20,
+                  child: SvgPicture.asset(ImageAssets.leftArrowIc),
+                ),
+                onTap: () {
+                  _pageController.animateToPage(_viewModel.goPrevios(),
+                      duration: Duration(milliseconds: DurationConstant.d300),
+                      curve: Curves.bounceInOut);
+                },
               ),
-              onTap: () {
-                _pageController.animateToPage(_getNextIndex(),
-                    duration: Duration(milliseconds: DurationConstant.d300),
-                    curve: Curves.bounceInOut);
-              },
             ),
-          ),
-        ],
-      ),
-    );
-  }
 
-  int _getPreviousIndex() {
-    int previousIndex = _currentIndex--;
-    if (previousIndex == -1) {
-      _currentIndex = _list.length - 1;
+            //circles indicator
+            Row(
+              children: [
+                for (int i = 0; i < slideViewObject.numOfSlides; i++)
+                  Padding(
+                    padding: EdgeInsets.all(AppPadding.p14),
+                    child: _getProperCircle(i, slideViewObject.currentIndex),
+                  )
+              ],
+            ),
+            //right arrow
+            Padding(
+              //left arrow
+              padding: EdgeInsets.all(AppPadding.p14),
+              child: GestureDetector(
+                child: SizedBox(
+                  height: AppSize.s20,
+                  width: AppSize.s20,
+                  child: SvgPicture.asset(ImageAssets.rightArrowIc),
+                ),
+                onTap: () {
+                  _pageController.animateToPage(_viewModel.goNext(),
+                      duration: Duration(milliseconds: DurationConstant.d300),
+                      curve: Curves.bounceInOut);
+                },
+              ),
+            ),
+          ],
+        ),
+      );
     }
-    return _currentIndex;
   }
 
-  int _getNextIndex() {
-    int nextIndex = _currentIndex++;
-    if (nextIndex >= _list.length) {
-      _currentIndex = 0;
-    }
-    return _currentIndex;
-  }
-
-  Widget _getProperCircle(int index) {
-    if (index == _currentIndex) {
+  Widget _getProperCircle(int index, int currentIndex) {
+    if (index == currentIndex) {
       return SvgPicture.asset(ImageAssets.hollowCircleIc);
     } else {
       return SvgPicture.asset(ImageAssets.solidCircleIc);
     }
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
 //-------------------------------------------------------------------
--- lib/presentation/onboarding/onboarding_view_model.dart
+++ lib/presentation/onboarding/onboarding_view_model.dart
@@ -3,12 +3,18 @@ import 'dart:async';
 import 'package:flutter_advanced_app/domain/model.dart';
 import 'package:flutter_advanced_app/presentation/base/base_view_model.dart';
 
+import '../resources/assets_manager.dart';
+import '../resources/strings_manager.dart';
+
 class OnBoardingViewModel extends BaseViewModel
     with OnBoardingViewModelInputs, OnBoardingViewModelOutputs {
   // stream controllers
   final StreamController _streamController =
       StreamController<SlideViewObject>();
-  //inputs
+  late final List<SliderObject> _list;
+  int _currentIndex = 0;
+
+  //inputs --------
   @override
   void dispose() {
     _streamController.close();
@@ -16,33 +22,68 @@ class OnBoardingViewModel extends BaseViewModel
 
   @override
   void start() {
-    // TODO: implement start
+    _list = _getSliderData();
+    _postDataToView();
   }
 
   @override
-  void goNext() {
-    // TODO: implement goNext
+  int goNext() {
+    int nextIndex = _currentIndex++;
+    if (nextIndex >= _list.length) {
+      _currentIndex = 0;
+    }
+    return _currentIndex;
   }
 
   @override
-  void goPrevios() {
-    // TODO: implement goPrevios
+  int goPrevios() {
+    int previousIndex = _currentIndex--;
+    if (previousIndex == -1) {
+      _currentIndex = _list.length - 1;
+    }
+    return _currentIndex;
   }
 
   @override
   void onPageChanged(int index) {
-    // TODO: implement onPageChanged
+    _currentIndex = index;
+    _postDataToView();
   }
 
   @override
   Sink get inputSliderViewObject => _streamController.sink;
 
+  //outputs
   @override
   Stream<SlideViewObject> get outputSliderViewObject =>
       _streamController.stream.map((slideViewObject) => slideViewObject);
+
+  //private functions
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          AppString.onBoardingTitle1,
+          AppString.onBoardingSubTitle1,
+          ImageAssets.onboardingLogo1,
+        ),
+        SliderObject(
+          AppString.onBoardingTitle2,
+          AppString.onBoardingSubTitle2,
+          ImageAssets.onboardingLogo2,
+        ),
+        SliderObject(
+          AppString.onBoardingTitle3,
+          AppString.onBoardingSubTitle3,
+          ImageAssets.onboardingLogo3,
+        ),
+      ];
+
+  _postDataToView() {
+    inputSliderViewObject.add(
+        SlideViewObject(_list[_currentIndex], _currentIndex, _list.length));
+  }
 }
 
-//orders the our view model will recieve from our view
+//orders the our view model will receive from our view
 mixin OnBoardingViewModelInputs {
   void goNext(); // when user click on right arrow or swipe left
   void goPrevios(); // when user click on left arrow or swipe right
@@ -56,6 +97,7 @@ mixin OnBoardingViewModelOutputs {
   Stream<SlideViewObject> get outputSliderViewObject;
 }
 
+//------------------------------------
 class SlideViewObject {
   SliderObject sliderObject;
   int numOfSlides;
